4d29d08ee1c5f509375a9305249f6d62be909264
Lưu Hoàng Minh <hoangminhhy2k5@gmail.com>
2026-02-06 09:25:32 +0700

    Initial commit

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 #Lay model
 model = YOLO("yolov8n.pt")
 #sd model
-model.train(data = "/Users/Documents/DL-Project/OD_DA2_NN_v1/data/dataset/ALLCAM1TO6.v2i.yolov8/data.yaml",
+model.train(data = "/kaggle/working/ALLCAM1TO6-2/data.yaml",
             epochs = 50,
             imgsz = 640,
             batch = 8,
